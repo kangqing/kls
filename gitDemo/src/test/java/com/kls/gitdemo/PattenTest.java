c8153a9fc7c04f5b932f8b85ac5c8fc15a9fdcc1
kangqing <kangqing.37@gmail.com>
2023-12-23 20:29:55 +0800

    equals

--- gitDemo/src/test/java/com/kls/gitdemo/PattenTest.java
+++ gitDemo/src/test/java/com/kls/gitdemo/PattenTest.java
@@ -16,4 +16,15 @@ public class PattenTest {
         final boolean foooood = ReUtil.isMatch("^[a-zA-Z0-9]{3}$", "1H");
         System.out.println(foooood);
     }
+
+    /**
+     * equals比内存地址，但是String类重写了，所以比的是值
+     */
+    @Test
+    void testEquals() {
+        String a = new String("123");
+        String b = "123";
+
+        System.out.println(a.equals(b));
+    }
 }
